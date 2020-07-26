53463620e53e7dd543d10d62ce0ef3ed6c5f4157
dcake19 <32597403+dcake19@users.noreply.github.com>
2020-07-26 14:42:10 +0100

    weather ids

---
+++ presentation-weather-2/src/main/java/com/example/presentation_weather_2/WeatherDayView.kt
@@ -0,0 +1,3 @@
+package com.example.presentation_weather_2
+
+data class WeatherDayView (val placeId: String,val placeName: String)
---
+++ presentation-weather-2/src/main/java/com/example/presentation_weather_2/WeatherId.kt
@@ -0,0 +1,64 @@
+package com.example.presentation_weather_2
+
+const val THUNDERSTORM_WITH_LIGHT_RAIN = 200
+const val THUNDERSTORM_WITH_RAIN = 201
+const val THUNDERSTORM_WITH_HEAVY_RAIN = 202
+const val THUNDERSTORM_LIGHT = 210
+const val THUNDERSTORM = 211
+const val THUNDERSTORM_HEAVY = 212
+const val THUNDERSTORM_RAGGED = 221
+const val THUNDERSTORM_WITH_LIGHT_DRIZZLE = 230
+const val THUNDERSTORM_WITH_DRIZZLE = 231
+const val THUNDERSTORM_WITH_HEAVY_DRIZZLE = 232
+
+const val DRIZZLE_LIGHT = 300
+const val DRIZZLE = 301
+const val DRIZZLE_HEAVY = 302
+const val DRIZZLE_RAIN_LIGHT = 310
+const val DRIZZLE_RAIN = 311
+const val DRIZZLE_RAIN_HEAVY = 312
+const val DRIZZLE_SHOWER_RAIN = 313
+const val DRIZZLE_HEAVY_SHOWER_RAIN = 314
+const val DRIZZLE_SHOWER = 321
+
+const val RAIN_LIGHT = 500
+const val RAIN_MODERATE = 501
+const val RAIN_hEAVY = 502
+const val RAIN_VERY_HEAVY = 503
+const val RAIN_EXTREME = 504
+const val RAIN_FREEZING = 511
+const val RAIN_LIGHT_SHOWER = 520
+const val RAIN_SHOWER = 521
+const val RAIN_HEAVY_SHOWER = 522
+const val RAIN_RAGGED_SHOWER = 531
+
+const val SNOW_LIGHT = 600
+const val SNOW = 601
+const val SNOW_HEAVY_ = 602
+const val SNOW_SLEET_ = 611
+const val SNOW_LIGHT_SHOWER_SLEET = 612
+const val SNOW_SHOWER_SLEET = 613
+const val SNOW_LIGHT_RAIN = 615
+const val SNOW_RAIN = 616
+const val SNOW_LIGHT_SHOWER = 620
+const val SNOW_SHOWER = 621
+const val SNOW_HEAVY_SHOWER = 622
+
+const val MIST = 701
+const val SMOKE = 702
+const val HAZE = 721
+const val DUST_WHIRLS = 731
+const val FOG = 741
+const val SAND = 751
+const val DUST = 761
+const val ASH = 762
+const val SQUALL = 771
+const val TORNADO = 781
+
+const val CLEAR = 800
+
+const val CLOUDS_FEW = 801
+const val CLOUDS_SCATTERED = 802
+const val CLOUDS_BROKEN = 803
+const val CLOUDS_OVERCAST = 804
+
